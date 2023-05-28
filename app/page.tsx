adf9dea6ca67185fb5317adb739a7b8c171b145b
andresgutgon <andresgutgon@gmail.com>
2023-05-28 12:21:06 +0200

    Add nothing to see here banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
-import crypto from "crypto"
-import { Bucket } from "sst/node/bucket"
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
-import UploadForm from "./(components)/UploadForm";
+import Image from 'next/image'
+/* import crypto from "crypto" */
+/* import { Bucket } from "sst/node/bucket" */
+/* import { getSignedUrl } from "@aws-sdk/s3-request-presigner"; */
+/* import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3" */
+/* import UploadForm from "./(components)/UploadForm"; */
 
 export default async function Home() {
-  const command = new PutObjectCommand({
-    ACL: "public-read",
-    Key: crypto.randomUUID(),
-    Bucket: Bucket.public.bucketName,
-  });
+  /* const command = new PutObjectCommand({ */
+  /*   ACL: "public-read", */
+  /*   Key: crypto.randomUUID(), */
+  /*   Bucket: Bucket.public.bucketName, */
+  /* }); */
   /* const signedUrl = await getSignedUrl(new S3Client({}), command) */
 
   return (
     <main className='pt-4 mx-auto max-w-xl'>
-      <UploadForm signedUrl={'NOP'} />
+      {/* <UploadForm signedUrl={'NOP'} /> */}
+      <Image width={500} height={286} src='https://i.gifer.com/origin/fc/fcde2ab4ec2c96a586f89345037e6ab9.gif' alt='Nothing to see here' />
     </main >
   )
 }
--- next.config.js
+++ next.config.js
@@ -7,6 +7,14 @@ const nextConfig = {
     config.experiments = { ...config.experiments, topLevelAwait: true }
     return config
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.gifer.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
